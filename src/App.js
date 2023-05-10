fb28899a88f86eef4177e98d4479ee496f257e07
mikevanis <hello@mikevanis.com>
2023-05-10 16:23:22 +0100

    manifest in input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [manifest, setManifest] = useState('')
+
+  const onManifestInputChange = (e) => {
+    setManifest(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Manifest URL: 
+        </label>
+        <input type='text' name='manifest' value={manifest} onChange={onManifestInputChange} placeholder='Manifest URL'/>
+      </form>
+      <esp-web-install-button
+        manifest={manifest}
+      ></esp-web-install-button>
     </div>
   );
 }
